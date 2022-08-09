d8cdae328857ff60908f70010148f4f17177c738
wooyong99 <wulovesk@naver.com>
2022-08-09 15:50:09 +0900

    명령어 코드 작성

--- src/com/jwy/exam/board/Main.java
+++ src/com/jwy/exam/board/Main.java
@@ -1,7 +1,16 @@
 package com.jwy.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.print("명령) ");
+    Scanner sc=new Scanner(System.in);
+    String input =sc.nextLine().trim();
+    System.out.printf("입력된 명령어: %s \n",input);
+    System.out.println("== 프로그램 종료 ==");
+    sc.close();
   }
 }
