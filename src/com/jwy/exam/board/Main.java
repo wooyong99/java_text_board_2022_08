4c313df6fabaee907099d35e862519b20fe78f4e
wooyong99 <wulovesk@naver.com>
2022-08-12 21:55:21 +0900

    파싱로직 패키지 생성 및 게시판 패키지 수정

--- src/com/jwy/exam/board/Main.java
+++ src/com/jwy/exam/board/Main.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import java.util.Scanner;
 //push 연습 1
 public class Main {
-  static int index_num=1;
+  // 마지막 게시글을 나타내기 위한 변수
+  static int last_index_num=1;
   static Map <Integer, Article> article=new HashMap();
+  // 테스트 입력 데이터 메서드
   static void CreateTestArticle(){
     CreateArticle("제목 1","내용 1");
     CreateArticle("제목 2","내용 2");
@@ -16,14 +18,43 @@ public class Main {
     CreateArticle("제목 5","내용 5");
     CreateArticle("제목 6","내용 6");
   }
-  static void ArticleDetail(int article_num){
-    System.out.println("== 게시판 상세보기 ==");
-    System.out.printf("번호 : %d\n제목 : %s\n내용 : %s\n",article.get(article_num).id,article.get(article_num).title,article.get(article_num).body);
-  }
+  // 게시글 생성 메서드
   static void CreateArticle(String title,String body){
-    article.put(index_num,new Article(index_num,title,body));
-    System.out.printf("%d번 게시물이 등록되었습니다.\n",index_num);
-    index_num++;
+    article.put(last_index_num,new Article(last_index_num,title,body));
+    System.out.printf("%d번 게시물이 등록되었습니다.\n",last_index_num);
+    last_index_num++;
+  }
+  // 최근 게시판 출력 메서드
+  static void ArticleDetail(){
+    System.out.println("== 최근 게시판 상세보기 ==");
+    System.out.printf("번호 : %d\n제목 : %s\n내용 : %s\n",article.get(last_index_num-1).id,article.get(last_index_num-1).title,article.get(last_index_num-1).body);
+  }
+  // 입력 번호 게시글 출력 메서드
+  static void ArticleDetail(int article_num)throws IndexOutOfBoundsException{
+    try{
+      if(article_num>article.size()){
+        throw new IndexOutOfBoundsException("마지막 게시글 번호는"+article.size()+"입니다");
+      }else{
+        System.out.println("== 게시판 상세보기 ==");
+        System.out.printf("번호 : %d\n제목 : %s\n내용 : %s\n",article.get(article_num).id,article.get(article_num).title,article.get(article_num).body);
+      }
+    }catch(IndexOutOfBoundsException e){
+      System.out.println(e.getMessage());
+    }
+  }
+  // 게시글 업데이트 메서드
+  static void Aricle_Update(int article_num,String update_title, String update_body){
+    try{
+      if(article_num>article.size()){
+        throw new IndexOutOfBoundsException("해당 번호 게시글이 존재하지 않습니다.");
+      }else{
+        Article select_article=article.get(article_num);
+        select_article.title=update_title;
+        select_article.body=update_body;
+      }
+    }catch(IndexOutOfBoundsException e){
+      System.out.println(e.getMessage());
+    }
   }
   public static void main(String[] args) {
     System.out.println("== 게시판 v 0.1 ==");
@@ -45,36 +76,63 @@ public class Main {
         System.out.printf("내용 : ");
         body=sc.nextLine();
         CreateArticle(title,body);
-      }else if(input.contains("/usr/article/detail/")){
+      }else if(input.contains("/usr/article/detail")){
         String[] article_num=input.split("/");
-        //if(input=="/usr/article/detail/"+)
-        ArticleDetail(Integer.parseInt(article_num[article_num.length-1]));
+        if(article_num[article_num.length-1].equals("detail")){
+          ArticleDetail();
+        }else{
+          ArticleDetail(Integer.parseInt(article_num[article_num.length-1]));
+        }
       }else if(input.equals("/usr/article/list")){
         System.out.println("== 게시물 리스트 ==");
         System.out.println("--------------------");
         System.out.println("번호 / 제목");
         System.out.println("--------------------");
-        for(int i=index_num-1;i>=1;i--){
+        for(int i=last_index_num-1;i>=1;i--){
           System.out.println(article.get(i).id+" / "+article.get(i).title);
         }
-      } else{
+      }else if(input.contains("/usr/article/update")){
+        String[] article_num=input.split("/");
+        try{
+          if(article_num[article_num.length-1].equals("update")==true){
+            throw new Exception("업데이트할 게시글 번호를 입력해주세요.");
+          } else if(last_index_num-1 < Integer.parseInt(article_num[article_num.length-1])){
+            throw new IndexOutOfBoundsException("마지막 게시글은 "+article.size()+"번입니다.");
+          }else{
+            System.out.println(last_index_num-1+" - "+article.size());
+            System.out.println(Integer.parseInt(article_num[article_num.length-1]));
+            System.out.println("변경할 제목을 입력해주세요 :");
+            String update_title = sc.nextLine();
+            System.out.println("변경할 내용을 입력해주세요 :");
+            String update_body = sc.nextLine();
+            Aricle_Update(Integer.parseInt(article_num[article_num.length - 1]), update_title, update_body);
+          }
+        }catch(IndexOutOfBoundsException e){
+          System.out.println(e.getMessage());
+        }catch(Exception e){
+          System.out.println(e.getMessage());
+        }
+      }
+      else{
         System.out.printf("입력된 명령어 : %s\n",input);
       }
     }
     System.out.println("== 프로그램 종료 ==");
-    System.out.println(article);
     sc.close();
   }
 }
+  // 게시글 Class
+  //
 class Article{
   int id;
-  String title;
-  String body;
+  String title, body;
+  // 매개변수 (아이디, 제목, 내용)를 갖는 생성자.
   Article(int id, String title, String body){
     this.id=id;
     this.title=title;
     this.body=body;
   }
+  // 출력 형태 : "id : 아이디 / title : 제목 / body : 내용"
   public String toString(){
     return "id : "+id+" / title : "+title+" / body : "+body;
   }
--- src/com/jwy/exam/urls/Main.java
+++ src/com/jwy/exam/urls/Main.java
@@ -1,40 +1,34 @@
 package com.jwy.exam.urls;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import sun.security.rsa.RSAUtil;
+
+import java.util.*;
 
 public class Main {
-  Map<String,Object> url=new HashMap(){{
-    put("title","");
-    put("body","");
-  }};
+  // Prameter의 이름과 값을 두개의 리스트로 분리.
+  static List<String> paramNames=new ArrayList();
+  static List<Object> paramValues=new ArrayList();
+  // 파싱 로직 :
+  // 1. 매개변수로 받은 url을  &를 기준으로 분리한다.
+  // 2. "="기호 미포함 및 2개 이상 / 공백 포함은 탐색하지 않는다.
+  // 3. 위 조건을 통과한 query는 이름과 값으로 나누기 위해 "=" 기준으로 분리한다.
+  // 4. 배열의 개수가 2개인 bit만을 리스트에 저장한다.
   public static void main(String args[]){
-    /*String queryString = "title=제목1&body=내용1";
-    String[] queryString_arr=queryString.split("&");*/
-    List<String> paramNames=new ArrayList();
-    List<Integer> paramValues=new ArrayList();
-    String queryString="a= &b=20&c=30&a=100";
+
+    String queryString="https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EC%A0%95%EC%9A%B0%EC%9A%A9";
     String[] query=queryString.split("&");
     for(String q:query){
-      if(q.contains("=")==false){
+      if(q.contains("=")==false || q.contains(" ")==true|| q.matches("[=]{2,}")==true){
         continue;
       }else {
         String[] bit = q.split("=");
-        if(bit[0]==(null)||bit[1]==(null)){
-          continue;
-        }else{
-          System.out.println(bit[0]);
-          System.out.println(bit[1]);
+        if(bit.length==2){
+          paramNames.add(bit[0]);
+          paramValues.add((Object)(bit[1]));
         }
       }
-     /* paramNames.add(bit[0]);
-      paramValues.add(Integer.parseInt(bit[1]));*/
-    }/*
-    for(int i=0;i<paramNames.size();i++){
-      System.out.println(paramNames.get(i));
-      System.out.println(paramValues.get(i));
-    }*/
+    }for(int i=0;i<paramNames.size();i++){
+      System.out.println("==== "+paramNames.get(i)+" - "+paramValues.get(i)+" =========");
+    }
   }
 }
